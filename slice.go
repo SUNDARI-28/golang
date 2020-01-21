9b94dc0773f1d43a327354b0432b5a37d9eaca37
SUNDARI-28 <60150951+SUNDARI-28@users.noreply.github.com>
2020-01-21 23:21:53 +0530

    Add files via upload

---
+++ slice.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main(){
+	/*
+	s := []int{2,3,4,5,6,7}
+	fmt.Println(s)
+
+	s = s[:0]
+	fmt.Println(s)
+	//printSlice(s)
+
+	s= s[1:4]
+	//printSlice(s)
+	fmt.Println(s)*/
+
+	var ch string="sundari"
+	fmt.Println(string(ch[0]))
+}
